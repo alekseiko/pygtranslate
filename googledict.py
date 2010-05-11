cc568b4e537172159613fecb4195c8b234365e74
alekseiko <alekseiko@gmx.com>
2010-05-12 01:48:17 +0400

    Add POC for work with google transale over AJAX service

--- googledict.py
+++ googledict.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import simplejson
+import urllib
 import sys
 
+GOOGLE_TRANSLATE_AJAX_URL = "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&langpair=en%7Cru&q=hello"
 def main(args):
+	print simplejson.load(urllib.urlopen(GOOGLE_TRANSLATE_AJAX_URL))["responseData"]["translatedText"]
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
